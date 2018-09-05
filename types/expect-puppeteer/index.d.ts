20f74eb060a5d063bffda64f06fd30104be4c9ae
Dan Schuman <danschuman@gmail.com>
2018-09-05 14:57:59 -0700

    Add toFillForm
    See https://github.com/smooth-code/jest-puppeteer/tree/master/packages/expect-puppeteer#expectinstancetofillformselector-values-options -- not currently present in typings

--- types/expect-puppeteer/index.d.ts
+++ types/expect-puppeteer/index.d.ts
@@ -57,6 +57,7 @@ declare global {
             toClick(selector: string, options?: ExpectToClickOptions): Promise<void>;
             toDisplayDialog(block: () => Promise<void>): Promise<Dialog>;
             toFill(selector: string, value: string, options?: ExpectTimingActions): Promise<void>;
+            toFillForm(selector: string, value: { [key: string]: any}, options?: ExpectTimingActions): Promise<void>;
             toMatch(selector: string, options?: ExpectTimingActions): Promise<void>;
             toMatchElement(selector: string, options?: ExpectToClickOptions): Promise<void>;
             toSelect(selector: string, valueOrText: string, options?: ExpectTimingActions): Promise<void>;
